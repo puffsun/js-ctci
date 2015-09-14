8c2fb8d7c0ce2386daa02937e68ff7cb4717006b
George Sun <sunwinner3@gmail.com>
2015-09-14 21:47:11 +0800

    Adding function to compare two linked lists.

--- spec/list_algorithms_spec.js
+++ spec/list_algorithms_spec.js
@@ -1,17 +1,57 @@
 "use strict";
 
-var list = require("../src/list_algorithms.js");
+var list = require("../src/lists.js");
+var algs = require("../src/list_algorithms.js");
 
 describe("Testing Linked List", function() {
 
+    describe("List equals", function() {
+        var l1, l2;
+        beforeEach(function() {
+            l1 = new list.SinglyNode('a');
+            l1.append('b');
+            l1.append('c');
+            l1.append('d');
+
+            l2 = new list.SinglyNode('a');
+            l2.append('b');
+            l2.append('c');
+            l2.append('d');
+        });
+
+        it("should equal for empty or null lists", function() {
+            expect(algs.list_equals(null, null)).toBeTruthy();
+            expect(algs.list_equals(undefined, null)).toBeTruthy();
+            expect(algs.list_equals(null, "")).toBeTruthy();
+            expect(algs.list_equals(undefined, "")).toBeTruthy();
+        });
+
+        it("should return true for two lists with same nodes", function() {
+            expect(algs.list_equals(l1, l2)).toBeTruthy();
+
+            l2.append('e');
+            expect(algs.list_equals(l1, l2)).toBeFalsy();
+
+            l1.append('f');
+            expect(algs.list_equals(l1, l2)).toBeFalsy();
+        });
+    });
+
     describe("Remove duplicates from unsorted linked list", function() {
+        var root;
+        beforeEach(function() {
+            root = new list.SinglyNode('a');
+            root.append('b');
+            root.append('c');
+            root.append('d');
+        });
+
         it("should return original node with empty or null", function() {
-            expect(list.dedup(undefined)).toBeUndefined();
-            expect(list.dedup(null)).toBeNull();
+            expect(algs.dedup(undefined)).toBeUndefined();
+            expect(algs.dedup(null)).toBeNull();
         });
 
         it("should dedup linked list", function() {
-            // TODO
         });
     });
 });
--- src/list_algorithms.js
+++ src/list_algorithms.js
@@ -1,5 +1,27 @@
 "use strict";
 
+function list_equals(node1, node2) {
+    if ((!node1 && !node2)) {
+        return true;
+    }
+
+    if (node1.length() !== node2.length()) {
+        return false;
+    }
+
+    var c1 = node1.head(),
+        c2 = node2.head();
+    while (c1 !== null && c2 !== null) {
+        if (c1.data !== c2.data) {
+            return false;
+        }
+        c1 = c1.next();
+        c2 = c2.next();
+    }
+
+    return true;
+}
+
 function dedup(root) {
     if (!root) {
         return root;
@@ -9,6 +31,7 @@ function dedup(root) {
 module.exports = (function() {
 
     return {
-        dedup      : dedup
+        dedup      : dedup,
+        list_equals : list_equals
     };
 }());
